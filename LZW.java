f1c007506d6de1a6060ebcfefd6003f2ae7c75c2
ktkaufman <70167517+ktkaufman@users.noreply.github.com>
2020-09-02 13:04:17 -0700

    Update LZW.java

--- LZW.java
+++ LZW.java
@@ -1,10 +1,15 @@
 public class LZW {
-	private HashMap<String, HashMap<Integer, String>> hash;
+	private HashMap<String, HashMap<Integer, String>> table;
 	private FileReader fr;
 
 	public LZW(String fileName) {
 		fr = new FileReader(fileName);
 		BufferedReader br = new BufferedReader(fr);
+	}
+	
+	public void fillTable()
+	{
+		
 	}
 
 }
