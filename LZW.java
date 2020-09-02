c7a0ab903ebca61e1d9f2360a34edbea4fc115a6
ktkaufman <70167517+ktkaufman@users.noreply.github.com>
2020-09-02 12:54:32 -0700

    Update LZW.java

--- LZW.java
+++ LZW.java
@@ -1,3 +1,10 @@
 public class LZW {
+	private ArrayList<String> table;
+	private FileReader fr;
+
+	public LZW(String fileName) {
+		fr = new FileReader(fileName);
+		BufferedReader br = new BufferedReader(fr);
+	}
 
 }
