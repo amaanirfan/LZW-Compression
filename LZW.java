b377d197ff2589786dc471758dde30a9eb7155ab
Isaiah Jeter <ijeter1@hwemail.com>
2020-09-02 13:11:33 -0700

    Update LZW.java

--- LZW.java
+++ LZW.java
@@ -1,15 +1,19 @@
 public class LZW {
-	private HashMap<String, HashMap<Integer, String>> table;
+	private HashMap<Integer, String>> table;
 	private FileReader fr;
 
 	public LZW(String fileName) {
 		fr = new FileReader(fileName);
 		BufferedReader br = new BufferedReader(fr);
+		table=new H
 	}
 	
 	public void fillTable()
 	{
-		
+		table=new HashMap<Integer, String>;
+		for(int n=0;n<127;n++){
+			table.put(n,(char)n);
+		}
 	}
 
 }
