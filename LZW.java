9da7f7e1f25aed5bd4550e5bf873bb80c6b1249d
ktkaufman <70167517+ktkaufman@users.noreply.github.com>
2020-09-02 13:12:43 -0700

    Update LZW.java

--- LZW.java
+++ LZW.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class LZW {
 	private HashMap<String, HashMap<Integer, String>> table;
 	private FileReader fr;
@@ -7,7 +8,12 @@ public class LZW {
 		BufferedReader br = new BufferedReader(fr);
 	}
 	
-	public void fillTable()
+	public void fillTable() //puts in all the values for one letter chars into the hash map
+	{
+		
+	}
+	
+	public ArrayList<Integer> encodedString (BufferedReader text)
 	{
 		
 	}
