74bcd912fc252776d838fe897d37754eec9d427c
ktkaufman <70167517+ktkaufman@users.noreply.github.com>
2020-09-02 13:13:34 -0700

    Update LZW.java

--- LZW.java
+++ LZW.java
@@ -11,15 +11,15 @@ public class LZW {
 	
 	public void fillTable() //puts in all the values for one letter chars into the hash map
 	{
-		
-	}
-	
-	public ArrayList<Integer> encodedString (BufferedReader text)
-	{
 		table=new HashMap<Integer, String>;
 		for(int n=0;n<127;n++){
 			table.put(n,(char)n);
 		}
+	}
+	
+	public ArrayList<Integer> encodeString (BufferedReader text)
+	{
+		
 	}
 
 }
