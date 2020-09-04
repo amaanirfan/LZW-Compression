b43544b4c62f6221b8cad4b69471e71d757864c6
ktkaufman <70167517+ktkaufman@users.noreply.github.com>
2020-09-04 11:06:40 -0700

    Update LZW.java

--- LZW.java
+++ LZW.java
@@ -1,51 +1,53 @@
+import java.io.*;
 import java.util.*;
 public class LZW {
 	private HashMap<String, Integer> table;
 	private FileReader fr;
+	private BufferedReader br;
 	private ArrayList<Integer> output;
 
-	public LZW(String fileName) {
+	public LZW(String fileName) throws FileNotFoundException {
 		fr = new FileReader(fileName);
-		BufferedReader br = new BufferedReader(fr);
-		table=new HashMap<Integer, String>();
+		br = new BufferedReader(fr);
+		table=new HashMap<String, Integer>();
 		output = new ArrayList<Integer>();
 	}
 	
 	public void fillTable() //puts in all the values for one letter chars into the hash map
 	{
-		table=new HashMap<Integer, String>;
+		table=new HashMap<String, Integer>();
 		for(int n=0;n<127;n++){
-			table.put(n,(char)n);
+			table.put(""+(char)n, n);
 		}
 	}
 	
-	public ArrayList<Integer> encodeString (BufferedReader text)
+	public ArrayList<Integer> encodeString () throws IOException
 	{
-		encode(text);
+		fillTable();
+		encode(br);
 		return output;
 	}
 	
-	public void encode(BufferedReader text) //goes through text, adds new patterns to hmap, and updates output with more integers
+	public void encode(BufferedReader text) throws IOException //goes through text, adds new patterns to hmap, and updates output with more integers
 	{
 		String temp = "";
 		int counter=127;
 		while (text.ready())//while ((i=br.read()) != -1){
 		{
-			temp+=char(text.read());
+			temp+=(char)(text.read());
 			if(!table.containsKey(temp)){
-				table.put(temp2,counter);
-				String temp2=temp.substring(0,temp.length()-1)
+				String temp2=temp.substring(0,temp.length()-1);
 				int tableIndex =table.get(temp2);
 				output.add(tableIndex);
 				counter++;
-				table.put(counter, temp);
+				table.put(temp, counter);
 				temp = temp.substring(temp.length()-1);
 			}
 		}
 	}
 
-	public toString(){
-		System.out.println(output);
+	public String toString(){
+		return ("" + output);
 	}
 
 }
