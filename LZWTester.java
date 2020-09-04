30ffba42483aeb140bfd66e03aef2c734bda8da3
ktkaufman <70167517+ktkaufman@users.noreply.github.com>
2020-09-04 11:08:55 -0700

    Create LZWTester.java

---
+++ LZWTester.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class LZWTester {
+	public static void main (String [] args) throws IOException
+	{
+		LZW test = new LZW("tester.txt");
+		test.encodeString();
+		System.out.println(test);
+	}
+	
+}
