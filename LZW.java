462b8f8bcdd237e606a61d84497dc94240372918
kcheng902 <47684688+kcheng902@users.noreply.github.com>
2020-09-18 09:09:17 -0700

    Improved LZW Encoder: see description
    (mostly in a previous commit) Our main improvement to the encoder was that, when possible, we converted the outputted integers to chars (so when the integer was < 256 because we're using UTF-8). This will improve the compression because chars take up less space than integers. Additionally, we changed the "magic number" of 127 to a constant and added in some more comments.

--- LZW.java
+++ LZW.java
@@ -5,22 +5,21 @@ public class LZW {
 	private FileReader fr;
 	private BufferedReader br;
 	private String newFileName;
+	private final int INIT_TABLE_SIZE = 127;
+	private final int EXTENDED_ASCII_SIZE = 256;
 
 	public LZW(String fileName) throws FileNotFoundException {
 		fr = new FileReader(fileName);
 		br = new BufferedReader(fr);
 		table=new HashMap<String, Integer>();
-		//newFileName = fileName+".lzw";
 		newFileName = "out.txt";
-		
-		
 		
 	}
 	
 	public void fillTable() //puts in all the values for one letter chars into the hash map
 	{
 		table=new HashMap<String, Integer>();
-		for(int n=0;n<127;n++){
+		for(int n=0;n<INIT_TABLE_SIZE;n++){
 			table.put(""+(char)n, n);
 		}
 	}
@@ -29,13 +28,12 @@ public class LZW {
 	{
 		fillTable();
 		encode(br);
-		
 	}
 	
 	public void encode(BufferedReader text) throws IOException //goes through text, adds new patterns to hmap, and updates output with more integers
 	{
 		String temp = "";//the temp variable keeps track of the series of letters you are reading so you can check if the variable is already present in you table or not
-		int counter=127;
+		int counter=INIT_TABLE_SIZE;
 		FileWriter out;
 		try {
 			out = new FileWriter(newFileName);
@@ -47,17 +45,17 @@ public class LZW {
 			if(!table.containsKey(temp)){ //if the table contains the doesn't have the series of letters already it adds the new pattern to the table and resets the temp variable to the the last letter of the pattern
 				String temp2=temp.substring(0,temp.length()-1);//temp 2 is a temporary placeholder that holds all the characters of temp besides the last one so that we can ouput the pattern which should be in the table already
 				int tableIndex =table.get(temp2);
-				if (tableIndex > 255) {
-					put.write(""+tableIndex+" ");
+				if (tableIndex >= EXTENDED_ASCII_SIZE) {
+					put.write(""+tableIndex+" "); //numbers not corresponding to ASCII table chars stay the same
 				} else {
-					put.write(""+(char)(tableIndex)+" ");
+					put.write(""+(char)(tableIndex)+" "); //numbers corresponding to ASCII table chars get converted to chars (less space)
 				}
 				counter++;
-				table.put(temp, counter);
-				temp = temp.substring(temp.length()-1);
+				table.put(temp, counter); //adds code to table
+				temp = temp.substring(temp.length()-1); //sets input string to last char
 			}	
 		}
-		put.write(""+table.get(temp));//writes the last code
+		put.write(""+table.get(temp)); //writes the last code
 		put.close();
 		} catch (IOException e) {
 			System.out.println("BIG OOPS");
