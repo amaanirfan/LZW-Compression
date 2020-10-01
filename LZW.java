8f44f74bbdad48638186d3947d17bdf6012da7c1
LWeet7 <liamweetman@gmail.com>
2020-10-01 13:21:31 -0700

    Weird Update

--- LZW.java
+++ LZW.java
@@ -38,30 +38,49 @@ public class LZW {
 	{
 		String temp = "";//the temp variable keeps track of the series of letters you are reading so you can check if the variable is already present in you table or not
 		int counter=INIT_TABLE_SIZE;
+		int tableIndex = 0;
 		FileWriter out;
+		String leastRecent = "";
 		try {
 			out = new FileWriter(newFileName);
 			BufferedWriter put = new BufferedWriter(out);
-		
 		while (text.ready())
 		{
+			
 			temp+=(char)(text.read());
 			if(!table.containsKey(temp)){ //if the table doesn't have the series of letters already it adds the new pattern to the table
 				String temp2=temp.substring(0,temp.length()-1);//temp 2 is a temporary placeholder that holds all the characters of temp besides the last one so that we can output the pattern which should be in the table already
-				int tableIndex =table.get(temp2);
+				System.out.println (temp);
+				System.out.println (temp2);
+				System.out.println (table.size());
+				System.out.println (homeQueue.size());
+				if (table.get(temp2) != null)
+				{
+					tableIndex =table.get(temp2);
+				}
+				else {
+					leastRecent = homeQueue.pollFirst();
+					table.remove(leastRecent);
+					counter--;
+					table.put(temp2, counter);
+					homeQueue.add(temp2);
+					counter++;
+					tableIndex = table.get(temp2);
+				}
 				if (tableIndex >= EXTENDED_ASCII_SIZE) {
 					put.write(""+tableIndex+" "); //numbers not corresponding to ASCII table chars stay the same
 				} else {
 					put.write(""+(char)(tableIndex)+" "); //numbers corresponding to ASCII table chars get converted to chars (less space)
 				}
-				homeQueue.placeToBack(temp);
-				counter++;
-				if (maxSize <= table.size()) {
-					String leastRecent = homeQueue.pollFirst();
+				homeQueue.placeToBack(temp2);
+				if (maxSize <= homeQueue.size()) {
+					leastRecent = homeQueue.pollFirst();
 					table.remove(leastRecent);
+					counter--;
 					//add this line in and find the last thing in the stack and delete it from the stack and earlier whenever we use an element from the table push that string to top of stack
 				}
 				table.put(temp, counter); //adds code to table
+				homeQueue.add(temp);
 				//queue.push(temp);
 				temp = temp.substring(temp.length()-1); //resets temp string to last char
 			}	
--- LZWTester.java
+++ LZWTester.java
@@ -2,10 +2,10 @@ import java.io.*;
 public class LZWTester {
 	public static void main (String [] args) throws IOException
 	{
-//		LZW test = new LZW("big.txt");
-//		test.createFile();
-		LZWDecoder decoder = new LZWDecoder();
-		decoder.decode("out.txt", "bigtest.txt");
+		LZW test = new LZW("message.txt", 4000);
+		test.createFile();
+		//LZWDecoder decoder = new LZWDecoder();
+		//decoder.decode("out.txt", "bigtest.txt");
 	}
 	
 }
